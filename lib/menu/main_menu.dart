ed173f2015b6fabcc097358be8a646072bb4eabb
Le Van Tien <lvtspkt@gmail.com>
2019-08-24 16:13:40 +0700

    + add action buttons on top.

--- lib/menu/main_menu.dart
+++ lib/menu/main_menu.dart
@@ -15,11 +15,27 @@ class MenuView extends StatefulWidget {
 }
 
 class _MenuViewState extends State<MenuView> {
+  final List<IconData> topIcons = [
+    Icons.search,
+    Icons.add
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        titleSpacing: 2.0,
         title: Text("Home"),
+        leading: IconButton(
+          icon: Icon(Icons.menu, size: 30, color: Colors.red,),
+          onPressed: () {},
+        ),
+        actions: List.generate(topIcons.length, (int index) {
+          return IconButton(
+            icon: Icon(topIcons[index], size: 30,),
+            onPressed: () {},
+          );
+        }).toList(),    
       ),
     );
   }
